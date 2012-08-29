548649adb2ab66d6a6a364f30cf52e5056a5ffb7
Greg Benison <gbenison@gmail.com>
2012-08-29 05:47:45 -0700

    Capified
    Set up Capistrano to pull from github and deploy to dreamhost.

---
+++ Capfile
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'railsless-deploy'
+
+load 'deploy' if respond_to?(:namespace) # cap2 differentiator
+Dir['vendor/gems/*/recipes/*.rb','vendor/plugins/*/recipes/*.rb'].each { |plugin| load(plugin) }
+
+load 'config/deploy' # remove this line to skip loading any of the default tasks
+
+namespace :deploy do
+  task :restart do
+  end
+end
---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+set :application, "hazel-photo"
+set :scm, :git
+set :repository,  "git://github.com/gbenison/hazel-photo.git"
+set :deploy_to,   "/home/gbenison/hazelphoto.com"
+
+set :user,     "gbenison"
+set :use_sudo, false
+
+role :web, "watanabe.dreamhost.com"
+role :app, "watanabe.dreamhost.com"
+role :db,  "watanabe.dreamhost.com"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
